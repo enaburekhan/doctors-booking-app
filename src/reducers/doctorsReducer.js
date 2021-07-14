c5c42f03935cf4de3d521791c9a270d84cf7fdbf
enaburekhan <ericenaburekhan2018@gmail.com>
2021-07-14 20:03:09 +0100

    Create doctors reducers

--- src/reducers/doctorsReducer.js
+++ src/reducers/doctorsReducer.js
@@ -1,14 +1,19 @@
+import { FETCH_DOCTORS_FAILURE, FETCH_DOCTORS_REQUEST, FETCH_DOCTORS_SUCCESS } from "../actions"
 
-export default function doctorsReducer(
-  state = {
-    loading: false,
-    all: [],    
-  }, action) {
+
+const initial_state = {
+  doctors: {},
+  isLoading: false    
+}  
+
+export default doctorsReducer = (initial_state, action) => {
     switch (action.type) {
-      case 'LOADING_DOCTOR_LISTS':
-        return { ...state, loading: true } 
-      case 'FETCH_DOCTOR_LISTS':
-        return { ...state, loading: false, all: action.payload } 
+      case FETCH_DOCTORS_REQUEST:
+        return { ...state, isLoading: true } 
+      case FETCH_DOCTORS_SUCCESS:
+        return { ...state, isLoading: false, doctors: action.payload }
+      case FETCH_DOCTORS_FAILURE:
+        return { ...state, isLoading: false, error: action.payload }   
       default:
         return state              
     }    
