2f6b762f0a3bdf003873796afb89868b80ec2dcd
enaburekhan <ericenaburekhan2018@gmail.com>
2021-07-24 18:00:51 +0100

    Create Loading component and add userReucer to store

---
+++ src/containers/Loading.js
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+const Loading = () => (<div className=""><FontAwesomeIcon icon={faSpinner} spin size="10x" /></div>);
+
+export default Loading;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import doctorsReducer from './doctorsSlice';
 import appointmentsReducer from './appointmentsSlice';
+import userReducer from './userSlice';
 
 export default configureStore({
   reducer: {
     doctors: doctorsReducer,
     appointments: appointmentsReducer,
+    user: userReducer,
 
   },
 });
