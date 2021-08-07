bbfc2236784486d0905ee2cd852289c34c11d72e
enaburekhan <ericenaburekhan2018@gmail.com>
2021-08-07 16:50:04 +0100

    Update Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
 const Home = () => (
   <div className="card style=width: 18rem listDoctors">
     <img src="https://user-images.githubusercontent.com/51296741/128604204-e71d0644-e88f-4743-b922-e708aa5a23d9.png" className="card-img-top" alt="home-page" />
-    <div className="card-body">
-      <h5 className="card-text">Book an appointment with a doctor</h5>
-    </div>
+    <div className="card-body" />
   </div>
 );
 
