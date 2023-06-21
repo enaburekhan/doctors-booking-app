2b2853aa653d06ec0df7449108786283e3be85d5
enaburekhan <ericenaburekhan2018@gmail.com>
2023-06-21 17:39:39 +0100

    Update api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,3 @@
-const API = 'http://localhost:3001/api/v1';
-
-// const API = 'https://doctors-booking-api-162q.onrender.com/api/v1';
+const API = 'https://enab-doctors-appointment-api.onrender.com/api/v1';
 
 export default API;
